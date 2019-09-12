60f47c0e24fa09d5e55b38f47e8977330d37b0e4
Jorge Tomé <jajtuco@gmail.com>
2019-09-11 22:04:40 -0300

    Seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,16 +8,34 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
+  equipo: any = [];
+
   cargada = false;
 
   constructor( private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
 
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
 
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
+
+    }  );
+  }
+
+  private cargarEquipo() {
+
+
+    this.http.get('https://angular-html-a6693.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
+      console.log(this.equipo);
     }  );
 
   }
